0b4feb792321f2d3fd503975df1fa6d4d67b396a
Ogulcan Asa <ogulcanasa@hotmail.com>
2023-06-13 21:53:20 +0300

    Create Project10_NamesToFaces

---
+++ Project10_NamesToFaces/Project10_NamesToFaces/PersonCellCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PersonCellCollectionViewCell.swift
+//  Project10_NamesToFaces
+//
+//  Created by Oğulcan Aşa on 13.06.2023.
+//
+
+import UIKit
+
+class PersonCellCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Project10_NamesToFaces/Project10_NamesToFaces/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Project10_NamesToFaces
+//
+//  Created by Oğulcan Aşa on 13.06.2023.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to dequeue PersonCell.")
+        }
+        cell.nameLabel.text = "deneme"
+        return cell
+    }
+}
+
