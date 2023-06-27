813fcd36a520bd5f8469d1401cecfb47c0a87200
Ogulcan Asa <ogulcanasa@hotmail.com>
2023-06-27 10:26:49 +0300

    Create Project16_CapitalCities

---
+++ Project16_CapitalCities/Project16_CapitalCities/Capital.swift
@@ -0,0 +1,21 @@
+//
+//  Capital.swift
+//  Project16_CapitalCities
+//
+//  Created by Oğulcan Aşa on 27.06.2023.
+//
+
+import MapKit
+import UIKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+
+    init(title: String? = nil, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
